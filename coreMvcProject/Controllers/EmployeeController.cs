0791dd4cf0227c4585a594d67fcf0ec73a0dada9
bgunwani <bhawna.gunwani@gmail.com>
2023-07-24 17:25:24 +0530

    Day One Final Commit

---
+++ coreMvcProject/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using coreMvcProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coreMvcProject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        static List<Employee> employees = new List<Employee>()
+        {
+            new Employee(){ Id=101, Name="King Kochhar", Age=23},
+            new Employee(){ Id=102, Name="Gautam Bhalla", Age=42},
+            new Employee(){ Id=103, Name="Sarah Bowling", Age=30},
+            new Employee(){ Id=104, Name="John Smith", Age=43},
+            new Employee(){ Id=105, Name="Roger Lee", Age=34},
+        };
+        public IActionResult Index()
+        {
+            return View(employees);
+        }
+        public IActionResult Details(int id)
+        {
+            /*var employee = employees[id];*/
+            var employee = employees.Find(x => x.Id == id);
+            return View(employee);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee employee) 
+        {
+            employees.Add(employee);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ coreMvcProject/Controllers/StudentController.cs
@@ -0,0 +1,50 @@
+﻿using coreMvcProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coreMvcProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        List<Student> students = new List<Student>()
+        {
+            new Student(){ Id=101, Name="King Kochhar", Age=23},
+            new Student(){ Id=102, Name="Gautam Bhalla", Age=42},
+            new Student(){ Id=103, Name="Sarah Bowling", Age=30},
+            new Student(){ Id=104, Name="John Smith", Age=43},
+            new Student(){ Id=105, Name="Roger Lee", Age=34},
+        };
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Student Management System";
+            ViewBag.StudentCount = students.Count();
+            ViewBag.Students = students;
+            return View();
+        }
+        public IActionResult Details()
+        {
+            ViewData["Message"] = "Student Management System";
+            ViewData["StudentCount"] = students.Count();
+            ViewData["Students"] = students;
+            return View();
+        }
+        public IActionResult Info()
+        {
+            TempData["Message"] = "Student Management System";
+            TempData["StudentCount"] = students.Count();
+            TempData["Students"] = students;
+            return View();
+        }
+        public IActionResult TestOne()
+        {
+            if (TempData["Message"] == null)
+                return RedirectToAction("Index", "Home");
+            ViewBag.Message = TempData["Message"];
+            return View();
+        }
+    }
+}
+
+
+/* The Keep Function: Used to preserve the data of TempData object even after the value is read. */
+
+/* The Peek Function: Used to readthe value without clearing it. */
